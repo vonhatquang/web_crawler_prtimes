f699e9c464aef27ddca57dd3969fb8c1a2ad463c
Quang <quang.vo@tpptechnology.com>
2023-06-30 06:05:27 +0700

    Update Source

--- resources/views/crawlerData.blade.php
+++ resources/views/crawlerData.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="Simple CMS"/>
     <meta name="author" content="Sheikh Heera"/>
     <meta name="csrf_token" content="{{ csrf_token() }}"/>
-    <title>情報収集(Google検索)</title>
+    <title>情報収集(PRTIMES)</title>
     <script src="{{ asset('jquery.js') }}"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -19,7 +19,16 @@
     </div>
 
     <div class="container keywords">
-        <h1 class="mb-2 text-center title fixed-top">Google検索キーワード</h1>
+        <div class="row justify-content-center">
+            <div class="col-12">   
+                <h1 class="text-center title fixed-top" style="margin-top:60px !important;">情報収集(PRTIMES)</h1>
+            </div> 
+        </div> 
+        <div class="row justify-content-center">
+            <div class="col-2">   
+                <h3 class="mb-2 text-left title" style="margin-left: -220px;">検索キーワード</h3>   
+            </div> 
+        </div> 
         <div class="row justify-content-center">
             <div class="col-6">
                 <input type="hidden" name="url" value="<?php echo url()->current();?>/">
@@ -27,19 +36,19 @@
                 <textarea class="form-control" id="searchKeyword" row="100" style="min-height:300px!important;max-height:300px!important"></textarea>
             </div> 
         </div>        
-        <div class="row error-div hidden">
+        <div class="row justify-content-center error-div hidden">
             <div class="col-6 container" style="margin-top:10px !important">
                 <div class="alert alert-danger error-message" role="alert" style="text-align: center;">
                     A simple danger alert—check it out!
                 </div>      
             </div> 
         </div>    
-        <div class="row download-div hidden">
+        <div class="row justify-content-center download-div hidden">
             <div class="col-6 container download" style="margin-top:10px !important; margin-left:400px !important">
             </div> 
         </div> 
-        <div class="row">
-            <div class="col-3 container" style="margin-top:10px !important">
+        <div class="row justify-content-center">
+            <div class="col-6 container" style="margin-top:10px !important">
                 <input type="button" value="クリア" id="cancel" class="btn btn-secondary">  
                 <input type="button" value="Webスクレイピング実行" id="crawler-btn" class="btn btn-primary">      
             </div> 
